2f28a1531529d8c601382a9b3e1434e5064a7f88
BaltasarOrtiz <baltasarinfo2019@gmail.com>
2023-09-26 01:06:03 -0300

    -
    Co-Authored-By: Nicolas Quiroga Santini <103652159+Nico-Qs@users.noreply.github.com>

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        <!-- Styles -->
+        @vite('resources/css/app.css')
+
+
+    </head>
+</head>
+<body>
+    <div class="relative bg-white overflow-hidden">
+        <!-- compon     ent -->
+        <div class="bg-white dark:bg-gray-900">
+            <div class="flex justify-center h-screen">
+                <div class="hidden bg-cover lg:block lg:w-2/3" style="background-image: url(https://images.unsplash.com/photo-1616763355603-9755a640a287?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80)">
+                    <div class="flex items-center h-full px-20 bg-gray-900 bg-opacity-40">
+                        <div>
+                            <h2 class="text-4xl font-bold text-white">Sistema de Reservas</h2>
+
+                            <p class="max-w-xl mt-3 text-gray-300">Lorem ipsum dolor sit, amet consectetur adipisicing elit. In autem ipsa, nulla laboriosam dolores, repellendus perferendis libero suscipit nam temporibus molestiae</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="flex items-center w-full max-w-md px-6 mx-auto lg:w-2/6">
+                    <div class="flex-1">
+
+                        <div class="text-center">
+                            <h2 class="text-4xl font-bold text-center text-gray-700 dark:text-white">Sistema de Reservas</h2>
+
+                            <p class="mt-3 text-gray-500 dark:text-gray-300">Ingresa para realizar tu reserva</p>
+                        </div>
+
+                        <div class="mt-8">
+                            <div class="flex justify-between mb-2">
+                                @if (Route::has('login'))
+                                    @auth
+                                        <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+                                    @else
+                                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+
+                                        @if (Route::has('register'))
+                                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                                        @endif
+                                    @endauth
+                                @endif
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
