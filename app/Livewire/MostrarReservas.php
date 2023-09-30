493afc054c4474dbc8737109dc132d99345d46da
BaltasarOrtiz <baltasarinfo2019@gmail.com>
2023-09-29 23:46:28 -0300

    -
    Co-Authored-By: Nicolas Quiroga Santini <103652159+Nico-Qs@users.noreply.github.com>

--- app/Livewire/MostrarReservas.php
+++ app/Livewire/MostrarReservas.php
@@ -14,8 +14,11 @@ class MostrarReservas extends Component
     public $finCalendario;
     public $fecha;
     public $anio;
+    public $diaMes;
     public $contMes = 1;
-    public $etiquetaDias = ['Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab', 'Dom'];
+    public $inicioSemana;
+    public $reservas;
+    public $diasSem = ['Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab', 'Dom'];
     public $meses = [
         1 => 'Enero',
         2 => 'Febrero',
@@ -36,30 +39,47 @@ class MostrarReservas extends Component
         $this->fechaActual = Carbon::now();
 
         $this->contMes = $this->fechaActual->copy()->format('n');
-        $this->inicioCalendario = $this->fechaActual->copy()->firstOfMonth()->startOfWeek();
-        $this->finCalendario = $this->fechaActual->copy()->lastOfMonth()->endOfWeek();
-        $this->fecha = $this->fechaActual->copy();
+        $this->inicioCalendario = $this->fechaActual->copy()->startOfMonth();
+        $this->finCalendario = $this->fechaActual->copy()->lastOfMonth();
         $this->anio = $this->fechaActual->copy()->year;
+        $this->inicioSemana = $this->fechaActual->copy()->firstOfMonth()->startOfWeek();
+        $this->diaMes = $this->inicioCalendario->copy()->startOfWeek();
+        $this->reservas = Reserva::select('fecha')
+            ->where('fecha', '>=', $this->inicioCalendario->format('Y-m-d'))
+            ->where('fecha', '<=', $this->finCalendario->format('Y-m-d'))
+            ->pluck('fecha')
+            ->toArray();
     }
 
     public function incrementar($objeto)
     {
         #Instancia con la fecha y mes de la vista
         $this->fechaActual =Carbon::createFromFormat('n/Y', $this->contMes.'/'.$this->anio);
-
         if($objeto == 'm'){
-            $this->inicioCalendario = $this->fechaActual->copy()->addMonth()->firstOfMonth();
+            $this->inicioCalendario = $this->fechaActual->copy()->addMonth()->startOfMonth();
             $this->finCalendario = $this->fechaActual->copy()->addMonth()->lastOfMonth();
 
             $this->contMes = $this->fechaActual->copy()->addMonth()->format('n');
             $this->anio =(int) $this->fechaActual->copy()->addMonth()->year;
+            $this->diaMes = $this->inicioCalendario->copy()->firstOfMonth()->startOfWeek();
+            $this->reservas = Reserva::select('fecha')
+            ->where('fecha', '>=', $this->inicioCalendario->format('Y-m-d'))
+            ->where('fecha', '<=', $this->finCalendario->format('Y-m-d'))
+            ->pluck('fecha')
+            ->toArray();
         } else {
             #Si es A es porque se incremente el año
-            $this->inicioCalendario = $this->fechaActual->copy()->addYear()->firstOfMonth();
+            $this->inicioCalendario = $this->fechaActual->copy()->addYear()->startOfMonth();
             $this->finCalendario = $this->fechaActual->copy()->addYear()->lastOfMonth();
 
             $this->contMes = $this->fechaActual->copy()->addYear()->format('n');
             $this->anio = (int) $this->fechaActual->copy()->addYear()->year;
+            $this->diaMes = $this->inicioCalendario->copy()->firstOfMonth()->startOfWeek();
+            $this->reservas = Reserva::select('fecha')
+            ->where('fecha', '>=', $this->inicioCalendario->format('Y-m-d'))
+            ->where('fecha', '<=', $this->finCalendario->format('Y-m-d'))
+            ->pluck('fecha')
+            ->toArray();
         }
     }
 
@@ -69,18 +89,30 @@ class MostrarReservas extends Component
         $this->fechaActual =Carbon::createFromFormat('n/Y', $this->contMes.'/'.$this->anio);
 
         if($objeto == 'm'){
-            $this->inicioCalendario = $this->fechaActual->copy()->subMonth()->firstOfMonth();
+            $this->inicioCalendario = $this->fechaActual->copy()->subMonth()->startOfMonth();
             $this->finCalendario = $this->fechaActual->copy()->subMonth()->lastOfMonth();
 
             $this->contMes = $this->fechaActual->copy()->subMonth()->format('n');
             $this->anio =(int) $this->fechaActual->copy()->subMonth()->year;
+            $this->diaMes = $this->inicioCalendario->copy()->firstOfMonth()->startOfWeek();
+            $this->reservas = Reserva::select('fecha')
+            ->where('fecha', '>=', $this->inicioCalendario->format('Y-m-d'))
+            ->where('fecha', '<=', $this->finCalendario->format('Y-m-d'))
+            ->pluck('fecha')
+            ->toArray();
         } else {
             #Si es A es porque se decrementa el año
-            $this->inicioCalendario = $this->fechaActual->copy()->subYear()->firstOfMonth();
+            $this->inicioCalendario = $this->fechaActual->copy()->subYear()->startOfMonth();
             $this->finCalendario = $this->fechaActual->copy()->subYear()->lastOfMonth();
 
             $this->contMes = $this->fechaActual->copy()->subYear()->format('n');
             $this->anio = (int) $this->fechaActual->copy()->subYear()->year;
+            $this->diaMes = $this->inicioCalendario->copy()->firstOfMonth()->startOfWeek();
+            $this->reservas = Reserva::select('fecha')
+            ->where('fecha', '>=', $this->inicioCalendario->format('Y-m-d'))
+            ->where('fecha', '<=', $this->finCalendario->format('Y-m-d'))
+            ->pluck('fecha')
+            ->toArray();
         }
     }
 
--- resources/views/livewire/mostrar-reservas.blade.php
+++ resources/views/livewire/mostrar-reservas.blade.php
@@ -8,14 +8,14 @@
             <div class="md:p-8 p-5 dark:bg-gray-600 bg-white rounded-t">
                 <div class="px-4 flex items-center justify-between">
                     <div class="flex items-center">
-                        <button aria-label="calendar backward" class="focus:text-gray-400 hover:text-gray-400 text-gray-800 dark:text-gray-100">
+                        <button aria-label="calendar backward" class="focus:text-gray-400 hover:text-gray-400 text-gray-800 dark:text-gray-100" wire:click="decrementar('m')">
                             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chevron-left" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                 <polyline points="15 6 9 12 15 18" />
                             </svg>
                         </button>
                         <span  tabindex="0" class="focus:outline-none  text-base font-bold dark:text-gray-100 text-gray-800">{{$meses[$contMes]}}</span>
-                        <button aria-label="calendar forward" class="focus:text-gray-400 hover:text-gray-400 ml-3 text-gray-800 dark:text-gray-100">
+                        <button aria-label="calendar forward" class="focus:text-gray-400 hover:text-gray-400 ml-3 text-gray-800 dark:text-gray-100" wire:click="incrementar('m')">
                             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler  icon-tabler-chevron-right" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                 <polyline points="9 6 15 12 9 18" />
@@ -23,14 +23,14 @@
                         </button>
                     </div>
                     <div class="flex items-center">
-                        <button aria-label="calendar backward" class="focus:text-gray-400 hover:text-gray-400 text-gray-800 dark:text-gray-100">
+                        <button aria-label="calendar backward" class="focus:text-gray-400 hover:text-gray-400 text-gray-800 dark:text-gray-100" wire:click="decrementar('')">
                             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chevron-left" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                 <polyline points="15 6 9 12 15 18" />
                             </svg>
                         </button>
                         <span  tabindex="0" class="focus:outline-none  text-base font-bold dark:text-gray-100 text-gray-800">{{$anio}}</span>
-                        <button aria-label="calendar forward" class="focus:text-gray-400 hover:text-gray-400 ml-3 text-gray-800 dark:text-gray-100">
+                        <button aria-label="calendar forward" class="focus:text-gray-400 hover:text-gray-400 ml-3 text-gray-800 dark:text-gray-100" wire:click="incrementar('')">
                             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler  icon-tabler-chevron-right" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                 <polyline points="9 6 15 12 9 18" />
@@ -39,13 +39,15 @@
                     </div>
                 </div>
                 <div class="flex items-center justify-between pt-8 overflow-x-auto">
+
+
                     <table class="w-full">
                         <thead>
                             <tr>
-                                @foreach($etiquetaDias as $dia)
+                                @foreach($diasSem as $diaS)
                                 <th>
                                     <div class="w-full flex justify-center">
-                                        <p class="text-base font-medium text-center text-gray-800 dark:text-gray-100">{{$dia}}</p>
+                                        <p class="text-base font-medium text-center text-gray-800 dark:text-gray-100">{{$diaS}}</p>
                                     </div>
                                 </th>
                                 @endforeach
@@ -53,28 +55,19 @@
                         </thead>
                         <tbody>
                             <tr>
-                                @for($i=1;$i<=30;$i++)
-                                <td>
-                                    <div class="w-full flex justify-center">
-                                        <p class="text-base font-medium text-center text-gray-800 dark:text-gray-100">{{$i}}</p>
-                                    </div>
-                                </td>
-                                @if(($i+7)%7==0)
-                            </tr>
-                            <tr>
-                                @endif
-                                @endfor
-                                @for($i=0;$i<6;$i++)
-                                <td>
-                                    <div class="w-full flex justify-center">
-                                        <p class="text-base font-medium text-center text-gray-800 dark:text-gray-100"></p>
-                                    </div>
-                                </td>
-                                @endfor
-                            </tr>
-                        </tbody>
-                            <tr>
-
+                                @while($diaMes <= $finCalendario)
+                                    <td>
+                                        <div class="w-full flex justify-center">
+                                            <p class="text-base font-medium text-center text-gray-800 dark:text-gray">{{$diaMes->day}}</p>
+                                        </div>
+                                    </td>
+                                    @if(($diaMes->dayOfWeek == 0) && ($diaMes != $finCalendario))
+                                        </tr><tr>
+                                    @endif
+                                    @php
+                                        $diaMes->addDay();
+                                    @endphp
+                                @endwhile
                             </tr>
                         </tbody>
                     </table>
